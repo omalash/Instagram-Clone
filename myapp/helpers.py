d7223711e04a23ce646ecb46c66b6b8d77832347
Omar Malash <omalash004@gmail.com>
2023-08-03 13:34:12 -0400

    Added a helper function for email validation

--- myapp/helpers.py
+++ myapp/helpers.py
@@ -1,4 +1,6 @@
 import re
+from pyisemail import is_email
+
 
 # a valid password is at least 8 letters long, contains at least one lowercase and uppercase character,
 # number, and special character
@@ -6,3 +8,6 @@ import re
 def is_valid_password(password):
     pattern = r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#\$%\^&\*\(\)\.]).{8,}$"
     return bool(re.match(pattern, password))
+
+def is_valid_email(email):
+    return is_email(email, check_dns=True)
